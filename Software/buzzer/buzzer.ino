549bde8b61fcc2ed4f8614d6520b91b135f47fe5
Mr-Hansen-Orewa <d.hansen@orewacollege.nz>
2024-02-02 11:37:02 +1300

    Update buzzer.ino

--- Software/buzzer/buzzer.ino
+++ Software/buzzer/buzzer.ino
@@ -3,6 +3,21 @@
 const byte BUTTONPIN = 2;
 const byte BUZZPIN = 3;
 
+boolean latch = false;
+
+const byte NOTE_A1  55;
+const byte NOTE_AS1 58;
+const byte NOTE_B1  62;
+const byte NOTE_C2  65;
+const byte NOTE_CS2 69;
+const byte NOTE_D2  73;
+const byte NOTE_DS2 78;
+const byte NOTE_E2  82;
+const byte NOTE_F2  87;
+const byte NOTE_FS2 93;
+const byte NOTE_G2  98;
+const byte NOTE_GS2 104;
+
 void setup() {
   pinMode(BUTTONPIN, INPUT);
   pinMode(BUZZPIN, OUTPUT);
@@ -12,11 +27,16 @@ void setup() {
 
 void loop() {
   if (digitalRead(BUTTONPIN) == HIGH) {
-    //digitalWrite(BUZZPIN, HIGH); // technically works, makes a click but tone is better
-    tone(BUZZPIN, 262); //262 is C4 frequency
+    latch = HIGH;
+    delay(250); //to debounce
   } else {
-    //digitalWrite(BUZZPIN, LOW); // as above tone is better
-    tone(BUZZPIN, 175); //175 is F3 frequency
+    latch = LOW;
+    delay(250); //to debounce
   }
-  delay(250);
+
+  if (latch == HIGH) {
+    tone(BUZZPIN, 262);  //262 is C4 frequency
+  } else {
+    notone();  //turn off tones
+  }
 }
